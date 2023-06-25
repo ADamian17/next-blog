333b51ff8147a268083efa5b21c975113c99013d
ADamian17 <adonisdesuvikdo@gmail.com>
2023-06-25 11:51:02 -0700

    reduce bundle size

--- src/components/posts/post-detail/post-content/index.tsx
+++ src/components/posts/post-detail/post-content/index.tsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import ReactMarkdown from "react-markdown";
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { oneDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+import oneDark from 'react-syntax-highlighter/dist/cjs/styles/prism/one-dark';
+import js from 'react-syntax-highlighter/dist/cjs/languages/prism/javascript';
+import css from 'react-syntax-highlighter/dist/cjs/languages/prism/css';
 
 import PostHeader from "../post-header";
 
 import styles from "./post-content.module.scss"
-import { ElementType } from "react";
+
+SyntaxHighlighter.registerLanguage('css', css)
+SyntaxHighlighter.registerLanguage('js', js)
 
 const PostContent: React.FC<Posts.PostContentProps> = ({ post }) => {
   const { title, image, slug, content } = post;
