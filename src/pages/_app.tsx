67a634e5e00be4a6198053c5e6adcbd0fb86396e
ADamian17 <adonisdesuvikdo@gmail.com>
2023-06-25 09:40:47 -0700

    updated styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
-import MainLayout from '@/layouts/main-layout'
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import MainLayout from '@/layouts/main-layout'
+import type { AppProps } from 'next/app'
+
+import '@/styles/globals.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
